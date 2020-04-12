6bfc5bfb9346a0453a8e3eacfadd8caadc29eb09
WuTongMu <1326771967@qq.com>
2020-04-12 08:58:23 +0800

    修改命名空间为LinFeng.Common

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("LinFengCommon")]
+[assembly: AssemblyTitle("LinFeng.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LinFengCommon")]
+[assembly: AssemblyProduct("LinFeng.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
